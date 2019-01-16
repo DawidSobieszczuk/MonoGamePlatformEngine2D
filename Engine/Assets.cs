4a7add61290cefb5c67da6a561f4e53256e29268
Dawid Sobieszczuk <dawidsobieszczuk@gmail.com>
2019-01-17 00:47:48 +0100

    Create Assets, and Extensions Class

---
+++ Engine/Assets.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework.Content;
+using MonoGame.Extended.Collections;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine {
+    /// <summary>
+    /// Przechowuje wszyskie asssety. Wczytaj asssety przed inicjalizacją świata
+    /// </summary>
+    class Assets {
+        ContentManager _content;
+        Dictionary<string, object> _assets;
+
+        public Assets(ContentManager content) {
+            _content = content;
+            _assets = new Dictionary<string, object>();
+        }
+
+
+        public T Add<T>(string name, T item) {
+            _assets.Add(name, item);
+            return item;
+        }
+
+        /// <summary>
+        /// Load from Content File
+        /// </summary>
+        public T Load<T>(string assetName) {
+            T item = _content.Load<T>(assetName);
+            _assets.Add(assetName, item);
+            return item;
+        }
+
+        public object Get(string assetName) => _assets[assetName];
+        public string GetName(object asset) => _assets.GetKey(asset);
+
+        public T Get<T>(string assetName) => (T)_assets[assetName];
+
+        public object this[string assetName] {
+            get => Get(assetName);
+        }
+    }
+}
---
+++ Engine/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine {
+    static class Extensions {
+        public static T GetKey<T, K>(this Dictionary<T, K> dictionary, K value) {
+            return dictionary.FirstOrDefault(x => x.Value.Equals(value)).Key;
+        }
+    }
+}
